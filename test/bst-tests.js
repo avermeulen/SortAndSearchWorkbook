87e1151528a3137d566744a6a77f2169a9f3b789
avermeulen <andre.vermeulen@gmail.com>
2016-07-13 18:26:24 +0200

    initial commit

---
+++ test/bst-tests.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+describe('The BST Object', function(){
+
+    it('should allow one to add values to it', function(){
+        assert.equal('1', '2');
+    });
+
+
+
+
+});
---
+++ test/linkedlist-foundation-tests.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+describe('To understand Linked Lists you', function(){
+
+    it('should be able to swap the 1st and 2nd values in a List', function(){
+        assert.equal('1', '2');
+    });
+
+    it('should be able to swap the 1st and last values in a List', function(){
+        assert.equal('1', '2');
+    });
+
+
+
+});
